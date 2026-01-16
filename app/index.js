e6935ffe388f4e0cf5ae0ab8be854c459c10d22e
DESKTOP-DB3FB27\user1 <wook's>
2026-01-16 16:27:05 +0900

    카드 입력 후 카드 생성

--- app/index.js
+++ app/index.js
@@ -1,1 +1,77 @@
-console.log("zz");
+
+function setListnerToCard(){
+    const cardAreaArr = document.querySelectorAll(".card-area");
+    for(const cardArea of cardAreaArr){
+        cardArea.addEventListener("click", function(evt){
+            evt.currentTarget.classList.toggle("flip");
+    
+        });
+    }
+}
+
+
+
+
+const main = document.querySelector("main");
+
+const str = `
+    <div   div class="card-area">
+        <div class="card">
+            <div class="card-back">뒷면</div>
+            <div class="card-front">앞면</div>
+        </div>
+    </div>
+`;
+
+
+function generateCardList(){
+    cardCnt = document.querySelector("#cardCnt").value;
+    main.innerHTML = "";
+    for(let i = 0; i < cardCnt; ++i){
+        main.innerHTML += str;
+    }
+}
+
+
+function handleClick(){
+    generateCardList();
+    setListnerToCard();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
